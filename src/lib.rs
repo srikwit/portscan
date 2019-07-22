95b34568f220880ef1c5138c2cde51cb7e77645a
Srikanth Suresh <4140019+srikwit@users.noreply.github.com>
2019-07-23 00:27:15 +0530

    Added supoort for TCP full scan

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use std::net::{IpAddr,TcpStream};
+use std::collections::HashMap;
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub enum PortState{
+Open,
+Filtered,
+Closed
+}
+
+
+pub fn port_scan(ip: IpAddr) -> HashMap<u16,PortState> {
+ let mut scan_data = HashMap::new();
+ for port in std::u16::MIN .. std::u16::MAX {
+     if let Ok(_stream) = TcpStream::connect((ip,port)) {
+         scan_data.insert(port,PortState::Open);
+        }
+     else{
+         continue;
+        }
+    }
+    scan_data
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
